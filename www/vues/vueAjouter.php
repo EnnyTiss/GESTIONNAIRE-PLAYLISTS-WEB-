f7cf4ce2cce0e976f66254f78dba259eb2f6751d
p2001246 <clarisse.paul-dauphin@etu.univ-lyon1.fr>
2022-11-18 13:08:03 +0100

    modif vue Ajouter

--- www/vues/vueAjouter.php
+++ www/vues/vueAjouter.php
@@ -1,8 +1,14 @@
 <h2>Ajout d'une chanson </h2>
-
+<!-- test-->
 <form method="post" action="#">
-	<label for="nomChanson">Nom de la Chanson : </label>
-	<input type="text" name="nomChanson" id="nomChanson" placeholder="Black Summer" required />
+	<label for="titreChanson">Nom de la Chanson : </label>
+	<input type="text" name="titreChanson" id="titreChanson" placeholder="Black Summer" required />
+	<br/>
+	<label for="dateChanson"> Date de sortie </label>
+	<input type="date" name="dateChanson" id="dateChanson" required/>
+	<br/>
+	<label for="minutes"> durée de Chanson : </label>
+	<imput type="number" name="minutes" id="minutes" required/> : <imput type="number" name="secondes" id="secondes" required/>
 	<br/><br/>
 	<input type="submit" name="boutonValider" value="Ajouter"/>
 </form>
@@ -10,4 +16,3 @@
 <?php if(isset($message)) { ?>
 	<p style="background-color: yellow;"><?= $message ?></p>
 <?php } ?>
-
